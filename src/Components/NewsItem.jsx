f9000e50349ea0f0eafbb54b6dcd3ad554edfd24
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-04 19:36:10 +0530

    create structure

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,16 @@
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div className="card" style={{ maxWidth: "345px" }}>
+      <img src={src} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
